cc5580bb38eb45923ee4c38a6969981c667de729
eroul-ri <askyws@gravityneocyon.com>
2021-12-02 14:00:13 +0900

    rest 프로젝트생성

---
+++ src/main/java/com/rest/myword/RestMywordApplication.java
@@ -0,0 +1,13 @@
+package com.rest.myword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMywordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestMywordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rest/myword/RestMywordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.myword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMywordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
